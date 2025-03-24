58af369df9089e3d91d65344fc5a588af9eba8a6
임재원 <nemo0824@naver.com>
2025-03-24 16:52:41 +0900

    refactor: pageInfo loadTime 삭제.

--- src/cores/pageInfo.ts
+++ src/cores/pageInfo.ts
@@ -9,19 +9,8 @@ export function sendPageReferrer() {
 }
 
 export function sendPageInfo() {
-  const loadTime = getPageLoadTime();
   const data = {
     url: window.location.href,
-    loadTime: loadTime,
   };
   sendToServer('/trackerSdk/pageInfo', data);
-}
-
-function getPageLoadTime() {
-  const [navigation] = performance.getEntriesByType(
-    'navigation'
-  ) as PerformanceNavigationTiming[];
-  return navigation
-    ? Math.floor(navigation.loadEventEnd - navigation.startTime)
-    : null;
 }
